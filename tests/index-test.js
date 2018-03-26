0c9872b7f6735b79af5db1474f108ff88f487036
Jeremy Wood <farachan@gmail.com>
2018-03-26 15:22:22 -0400

    Fixed build issues with tests.

--- tests/index-test.js
+++ tests/index-test.js
@@ -1,23 +1,23 @@
-import expect from 'expect'
-import React from 'react'
-import {render, unmountComponentAtNode} from 'react-dom'
+import expect from 'expect';
+import React from 'react';
+import { render, unmountComponentAtNode } from 'react-dom';
 
-import Component from 'src/'
+import { AccessibleReactTable } from '../src/';
 
-describe('Component', () => {
-  let node
+describe('AccessibleReactTable', () => {
+  let node;
 
   beforeEach(() => {
-    node = document.createElement('div')
-  })
+    node = document.createElement('div');
+  });
 
   afterEach(() => {
-    unmountComponentAtNode(node)
-  })
+    unmountComponentAtNode(node);
+  });
 
   it('displays a welcome message', () => {
-    render(<Component/>, node, () => {
-      expect(node.innerHTML).toContain('Welcome to React components')
-    })
-  })
-})
+    render(<AccessibleReactTable />, node, () => {
+      expect(node.innerHTML).toContain('Welcome to React components');
+    });
+  });
+});
